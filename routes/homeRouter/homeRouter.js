eb72c1f049436619ada73b4cd568c40db5cb753e
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-03-11 15:19:07 +0800

    feat: create a route for folder CRUD operation

--- routes/homeRouter/homeRouter.js
+++ routes/homeRouter/homeRouter.js
@@ -4,6 +4,12 @@ const homeController = require("../../controllers/homeController/homeController"
 
 homeRouter.get("/", homeController.home);
 homeRouter.post("/sign-up", homeController.signUp);
+homeRouter.post("/upload-file", homeController.postFileUpload);
 homeRouter.post("/create-folder", homeController.postCreateFolder);
+homeRouter.post(
+  "/delete-folder/:id/:folderName",
+  homeController.postDeleteFolder
+);
+homeRouter.post("/update-folder", homeController.postUpdateFolder);
 
 module.exports = homeRouter;
