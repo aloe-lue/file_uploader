1f022d3c62602246012177bfb52593c3dc858e04
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-03-14 15:43:47 +0800

    feat: remove the routes for uploading file on the homepage route

--- routes/homeRouter/homeRouter.js
+++ routes/homeRouter/homeRouter.js
@@ -4,7 +4,10 @@ const homeController = require("../../controllers/homeController/homeController"
 
 homeRouter.get("/", homeController.home);
 homeRouter.post("/sign-up", homeController.signUp);
-homeRouter.post("/upload-file", homeController.postFileUpload);
+
+// remove this i don't have to upload file in the root
+// homeRouter.post("/upload-file", homeController.postFileUpload);
+
 homeRouter.post("/create-folder", homeController.postCreateFolder);
 homeRouter.post(
   "/delete-folder/:id/:folderName",
