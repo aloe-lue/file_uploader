16ed198dfb59ddaf1952436640eecbaa994a6d21
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-03-17 18:09:04 +0800

    fix: shorten the routes to deleting folder

--- routes/homeRouter/homeRouter.js
+++ routes/homeRouter/homeRouter.js
@@ -9,10 +9,7 @@ homeRouter.post("/sign-up", homeController.signUp);
 // homeRouter.post("/upload-file", homeController.postFileUpload);
 
 homeRouter.post("/create-folder", homeController.postCreateFolder);
-homeRouter.post(
-  "/delete-folder/:id/:folderName",
-  homeController.postDeleteFolder
-);
+homeRouter.post("/delete-folder", homeController.postDeleteFolder);
 homeRouter.post("/update-folder", homeController.postUpdateFolder);
 
 module.exports = homeRouter;
