dcff13f5679e9e2795a4b3a4063b768e10527816
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-04-04 15:41:04 +0800

    fix: folder routes should be consistent
    after #=> /folder/:folderName/action/id/:id

--- routes/folderRouter/folderRouter.js
+++ routes/folderRouter/folderRouter.js
@@ -2,12 +2,34 @@ const { Router } = require("express");
 const folderRouter = Router();
 const folderController = require("../../controllers/folderController/folderController");
 
+// upload route
 folderRouter.post(
   "/:folder/upload-file/id/:id",
   folderController.postUploadFile
 );
-folderRouter.get("/:folder/read-file/id/:id", folderController.getReadFolder);
-folderRouter.post("/:folder/update-file/:id", folderController.postUpdateFile);
-folderRouter.post("/:folder/delete-file/:id", folderController.postDeleteFile);
+
+// routes the content to folder files
+folderRouter.get(
+  "/:folder/read-file/id/:id",
+  folderController.getReadFolderFile
+);
+
+// update the file
+folderRouter.post(
+  "/:folder/update-file/id/:id",
+  folderController.postUpdateFile
+);
+
+// delete the file
+folderRouter.post(
+  "/:folder/delete-file/id/:id",
+  folderController.postDeleteFile
+);
+
+// download file
+folderRouter.get(
+  "/:folder/download-file/id/:id",
+  folderController.getDownloadFile
+);
 
 module.exports = folderRouter;
