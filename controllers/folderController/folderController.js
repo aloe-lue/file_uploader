e80219df39b8c4ab4c774e57cef46351e645d6cc
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-04-05 13:23:33 +0800

    feat: use the node js downloader to download a file to server
    purpose #=> download the image using the url of the uploaded image
    to cloudinary.
    
    purpose #=> add another controller for renaming a file name
    => file is not added a format on so do it later or never.

--- controllers/folderController/folderController.js
+++ controllers/folderController/folderController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../../prisma/query");
-const { param, validationResult } = require("express-validator");
+const { body, param, validationResult, query } = require("express-validator");
 const multer = require("multer");
 const path = require("node:path");
 const Mimetics = require("mimetics");
@@ -8,6 +8,7 @@ const fs = require("node:fs");
 const crypto = require("crypto");
 require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
+const { Downloader } = require("nodejs-file-downloader");
 
 cloudinary.config({
   secure: true,
@@ -20,7 +21,7 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, "storage"));
   },
   filename: function (req, file, cb) {
-    console.log(req.file);
+    // console.log(req.file);
     cb(null, `${crypto.randomUUID()}`);
   },
 });
@@ -67,11 +68,7 @@ const handleEmptyFile = function (req, res, next) {
 };
 
 // receive only image type
-const handleFakeMime = asyncHandler(async function handleFakeMime(
-  req,
-  res,
-  next
-) {
+const handleFakeMime = asyncHandler(async (req, res, next) => {
   const mimetics = new Mimetics();
   const filePath = req.file.path;
   const buffer = fs.readFileSync(filePath);
@@ -120,9 +117,7 @@ exports.postUploadFile = [
         console.error(error);
       });
 
-    // delete from storage
-    fs.unlinkSync(req.file.path);
-
+    // store the file data to the db
     await db.updateFileToFolder({
       folderName: req.params.folder,
       id: Number(req.params.id),
@@ -132,7 +127,11 @@ exports.postUploadFile = [
       size: Number(req.file.size),
       uploadedAt: new Date(),
       publicId: uploadResult.public_id,
+      randomName: `${uploadResult.public_id}.${uploadResult.format}`,
     });
+
+    // delete from storage
+    fs.unlinkSync(req.file.path);
 
     // after uploaded redirect user to where client uploading was before
     res.redirect(`/folder/${req.params.folder}/read-file/id/${req.params.id}`);
@@ -168,16 +167,17 @@ const readFolderVc = [
     .trim()
     .notEmpty()
     .withMessage(`id ${id}`)
-    // use the custom
+    // checks if the folder exist
     .custom(isFolderExist)
     .withMessage("folder doesn't exist"),
 ];
 
-exports.getReadFolder = [
+exports.getReadFolderFile = [
   readFolderVc,
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
 
+    // if there's no such thing as the folder name then redirect with a status of 400
     if (!errors.isEmpty()) {
       return res.status(400).redirect("/");
     }
@@ -195,12 +195,153 @@ exports.getReadFolder = [
   }),
 ];
 
-exports.postUpdateFile = (req, res) => {};
-exports.postDeleteFile = asyncHandler(async (req, res) => {
-  // you want to delete the file that you have uploaded hmmmm yeah probably
-  // const deleteResult = await cloudinary.
-  // render the user to the client roots
-  // single image file
+const fileIdUpdate = "should be an id a number";
+const fileRename = "should be a alphaNumeric";
 
-  const deleteResult = await cloudinary.uploader.destroy();
-});
+const postUpdateFileVC = [
+  body("fileIdUpdate")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage(`file id ${fileIdUpdate}`),
+  body("fileRename")
+    .trim()
+    .notEmpty()
+    .isString()
+    .isAlphanumeric()
+    .withMessage(`file rename ${fileRename}`),
+];
+
+exports.postUpdateFile = [
+  postUpdateFileVC,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const folderFiles = await db.readFileFolder({
+        folderName: req.params.folder,
+        folderId: Number(req.params.id),
+      });
+      // show the renaming errors
+      return res.status(400).render("folderView/folder", {
+        updateFileError: errors.array(),
+        deleteFileError: [],
+        fileUrlError: [],
+        fileError: "",
+        folderName: req.params.folder,
+        folderId: req.params.id,
+        folderFiles: folderFiles.file,
+      });
+    }
+
+    // rename the file name
+    const { fileIdUpdate, fileRename } = req.body;
+    await db.updateFile({ id: Number(fileIdUpdate), rename: fileRename });
+    res.redirect(`/folder/${req.params.folder}/read-file/id/${req.params.id}`);
+  }),
+];
+
+const publicId = "should not be modified";
+const fileIdDelete = "should be a number and not modified";
+const deleteFileVC = [
+  body("publicId")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage(`public id ${publicId}`),
+  body("fileIdDelete")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage(`file id ${fileIdDelete}`),
+];
+
+// deleting file controller middlewares
+exports.postDeleteFile = [
+  deleteFileVC,
+  asyncHandler(async (req, res) => {
+    // you want to delete the file that you have uploaded hmmmm yeah probably
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const folderFiles = await db.readFileFolder({
+        folderName: req.params.folder,
+        folderId: Number(req.params.id),
+      });
+      // the input has been modified
+      return res.status(400).render("folderView/folder", {
+        fileUrlError: errors.array(),
+        deleteFileError: errors.array(),
+        fileError: "",
+        folderName: req.params.folder,
+        folderId: req.params.id,
+        folderFiles: folderFiles.file,
+      });
+    }
+
+    // delete from cloudinary
+    const { publicId, fileIdDelete } = req.body;
+    await cloudinary.uploader.destroy(publicId, {
+      invalidate: true,
+    });
+
+    // delete the file info from db
+    await db.deleteFile({ publicId: publicId, id: Number(fileIdDelete) });
+    res.redirect(`/folder/${req.params.folder}/read-file/id/${req.params.id}`);
+  }),
+];
+
+const fileUrlMsg = "should be a url that is served with cdn";
+
+const downloadFileVC = [
+  query("fileUrl")
+    .trim()
+    .notEmpty()
+    .isURL()
+    .withMessage(`file url ${fileUrlMsg}.`),
+];
+
+exports.getDownloadFile = [
+  downloadFileVC,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // file url errors
+
+      const folderFiles = await db.readFileFolder({
+        folderName: req.params.folder,
+        folderId: Number(req.params.id),
+      });
+      return res.status(400).render("folderView/folder", {
+        fileUrlError: errors.array(),
+        fileError: "",
+        deleteFileError: [],
+        folderName: req.params.folder,
+        folderId: req.params.id,
+        folderFiles: folderFiles.file,
+      });
+    }
+
+    // get from query
+    const fileUrl = req.query.fileUrl;
+
+    // downloads directory
+    const downloadDir = path.join(__dirname, "download");
+
+    // downloads the file to server #=> helps with streams and write
+    const downloader = new Downloader({
+      url: fileUrl,
+      directory: downloadDir,
+    });
+
+    // use the file path to download from server then delete later
+    const { filePath /** downloadStatus */ } = await downloader.download();
+
+    res.download(filePath, function (err) {
+      // delete the file after download
+      // callback is async so it prefer sync at this point
+      fs.unlinkSync(filePath);
+    });
+  }),
+];
